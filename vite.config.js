210209aa53d8722ec47b2e57eb294f07c7a6dfd8
jhpark <jhpark@intellivix.ai>
2025-11-24 16:51:15 +0900

    Vue 서버 로컬호스트 이외에 IP 입력으로 접속 가능하도록 수정

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true, // 네트워크를 통해 접근 가능하도록 설정
+    port: 3000, // 원하는 포트 번호 (기본값: 5173)
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
